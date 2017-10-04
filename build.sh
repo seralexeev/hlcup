a74c8cb07ad0a0477c3a754fb6b4514ca73031a9
Sergey Alexeev <salexeev@golamago.com>
2017-10-04 17:41:01 +0300

    test

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker build -t richbitch .
-docker tag richbitch stor.highloadcup.ru/travels/modern_cat
-docker push stor.highloadcup.ru/travels/modern_cat
+docker tag richbitch stor.highloadcup.ru/travels/helpful_shark
+docker push stor.highloadcup.ru/travels/helpful_shark
