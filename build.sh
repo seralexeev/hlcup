b4399b3deee1acd1de260320cbfa98b333e323e6
Sergey Alexeev <ser.alexeev@gmail.com>
2017-10-05 23:31:37 +0300

    wip

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker build -t richbitch .
-docker tag richbitch stor.highloadcup.ru/travels/first_mouse
-docker push stor.highloadcup.ru/travels/first_mouse
+docker tag richbitch stor.highloadcup.ru/travels/$DOCKER_IMAGE
+docker push stor.highloadcup.ru/travels/$DOCKER_IMAGE
